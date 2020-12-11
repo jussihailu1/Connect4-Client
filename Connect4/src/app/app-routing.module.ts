cae5fbfb7f2f6c385707fab629a0d53fd10fd92b
Jussi <jussihailu1@gmail.com>
2020-12-11 02:11:06 +0100

    login page & home page layout setup

--- Connect4/src/app/app-routing.module.ts
+++ Connect4/src/app/app-routing.module.ts
@@ -1,10 +1,25 @@
+import { GameComponent } from './game/game.component';
+import { HomeComponent } from './home/home.component';
+import { LoginComponent } from './login/login.component';
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: "login", component: LoginComponent},
+  {path: "home", component: HomeComponent},
+  {path: "game", component: GameComponent},
+  {path: "", redirectTo: "/login", pathMatch: "full"},
+  // {path: "**", component: LoginComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
   exports: [RouterModule]
 })
 export class AppRoutingModule { }
+
+export const RoutingComponents = [
+  LoginComponent, 
+  HomeComponent,
+  GameComponent
+]
--- Connect4/src/app/app.module.ts
+++ Connect4/src/app/app.module.ts
@@ -1,16 +1,14 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
-import { AppRoutingModule } from './app-routing.module';
+import { AppRoutingModule, RoutingComponents } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { LoginComponent } from './login/login.component';
-import { HomeComponent } from './home/home.component';
+import { GameComponent } from './game/game.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    LoginComponent,
-    HomeComponent
+    RoutingComponents
   ],
   imports: [
     BrowserModule,
